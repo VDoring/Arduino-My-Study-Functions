c91e6180c4b7af10ff2e3072f1af133c1fafff55
Doring <hojun.dev@gmail.com>
2020-10-14 14:31:25 +0900

    ESP-01 Light Server ver 1

---
+++ Internet/Wi-Fi/ESP-01/Web Server/ESP01_OLED_WiFi_Pocket_Server_LightVer.ino.ino
@@ -0,0 +1,32 @@
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+const char* ssid = "U+Net340F"; //SSID
+const char* password = "018A015635"; //PW (PW가 없다면 공란으로 두기)
+WiFiServer server(80);
+
+void setup() {
+    Serial.begin(9600);
+    delay(100);
+    
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+    Serial.print("...");
+    WiFi.begin(ssid, password);
+
+    while(WiFi.status() != WL_CONNECTED) {} //와이파이 연결이 정상적일시 while 탈출
+    
+    Serial.println();
+    Serial.println("Connected!");
+
+    server.begin();
+    Serial.println("Server ON");
+    Serial.print("IP:");
+    Serial.print(WiFi.localIP());
+}
+
+void loop() {
+
+}
